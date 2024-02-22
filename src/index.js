458073f728f9b5c6b40b44e98d881700b8590bf7
Ragy4a <122722327+Ragy4a@users.noreply.github.com>
2024-02-22 16:12:06 +0200

    Production Version #1

---
+++ src/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function Array (...args) {
+    this.length = 0;
+    if (args.length) {
+        for (let i = 0; i < args.length; i++) {
+            this.push(args[i]);
+        }
+    }
+};
+
+Array.prototype = new ArrayProto();
+
+function ArrayProto () {
+    this.push = function () {
+        if(arguments) {
+            for (let i = 0; i < arguments.length; i++) {
+                this[this.length++] = arguments[i];
+            }
+        }
+        return this.length;
+    };
+
+    this.reduceRight = function(callback) {
+        let accumulator = this[this.length - 1];
+        let i = this.length - 2;
+        for (; i >= 0; i--) {
+            if (i in this) {
+                accumulator = callback(accumulator, this[i], i, this);
+            }
+        }
+        return accumulator;
+    };
+};
+
+
+const testArr = new Array(1, 2, 3, 4, 5);
+const result = testArr.reduceRight(function(accumulator, currentNumber) {
+    return accumulator + currentNumber;
+}, 1);
+
+console.log(result);

0db2068a8593417285c216190ca3af89a3860efb
Ragy4a <122722327+Ragy4a@users.noreply.github.com>
2024-02-29 13:59:13 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 'use strict';
 
-function MyArray (...args) {
+function MyArray(...args) {
     this.length = 0;
     if (args.length) {
         for (let i = 0; i < args.length; i++) {
             this.push(args[i]);
         }
     }
-};
+}
+
+function isMyArray(obj) {
+    return obj instanceof MyArray;
+}
 
 MyArray.prototype = new MyArrayProto();
 
-function MyArrayProto () {
+function MyArrayProto(...args) {
     this.push = function () {
-        if(arguments) {
-            for (let i = 0; i < arguments.length; i++) {
-                this[this.length++] = arguments[i];
+        if(args) {
+            for (let i = 0; i < args.length; i++) {
+                this[this.length++] = args[i];
             }
         }
         return this.length;
     };
 
-    this.reduceRight = function(callback, initialValue) {
+    this.forEach = function (callback) {
+		for (let i = 0; i < this.length; i++) {
+			callback(this[i], i, this);
+		}
+	};
+
+    this.concat = function (...args) {
+		const res = new MyArray();
+        this.forEach(el => {
+            res.push(el);
+        });
+		for (let i = 0; i < args.length; i++) {
+			if (Array.isArray(args[i])) {
+				res.push(...args[i]);
+			} else if (isMyArray(args[i])) {
+				for (let j = 0; j < args[i].length; j++) {
+					res.push(args[i][j]);
+				}
+			} else {
+				res.push(args[i]);
+			}
+		}
+		return res;
+	};
+
+    this.myReduceRight = function(callback, initialValue) {
         let accumulator = initialValue;
         let i = this.length - 1;
         if (!initialValue) {
@@ -29,16 +58,23 @@ function MyArrayProto () {
             i = this.length - 2;
         }
         for (; i >= 0; i--) {
-            accumulator = callback.call(accumulator, this[i], i, this);
+            accumulator = callback(accumulator, this[i], i, this);
         }
         return accumulator;
     };
+    
+    this.myFlat = function(depth = 1) {
+        let flattenedArray = new MyArray();
+        function flatten (arrProto, depth) {
+            for(let item of arrProto) {
+                if (MyArray.isMyArray(item) && depth) {
+                    flatten(item, depth - 1)
+                } else {
+                    flattenedArray.push(item);
+                }
+            }
+        }
+        flatten(this, depth);
+        return flattenedArray;
+    };
 };
-
-
-const testArr = new Array(1, 2, 3, 4, 5);
-const result = testArr.reduceRight(function(accumulator, currentNumber) {
-    return accumulator + currentNumber;
-}, 1);
-
-console.log(result);
